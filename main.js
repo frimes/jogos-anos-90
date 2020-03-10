e2842e1c4830f12f23908da7071084b8256668e2
luanpires94 <luan.94pires@gmail.com>
2020-03-10 16:12:06 -0300

    adicionando menu js e ajustes em geral no css

---
+++ main.js
@@ -0,0 +1,13 @@
+ 
+function handleMenu(){
+    if (document.getElementById('menu').style.display === 'flex'){
+        document.getElementById('menu').style.display = 'none'
+        document.getElementById('menu-hamburger').style.display = 'flex'
+        document.getElementById('close-hamburger').style.display = 'none'
+    }
+    else{
+        document.getElementById('menu').style.display = 'flex'
+        document.getElementById('menu-hamburger').style.display = 'none'
+        document.getElementById('close-hamburger').style.display = 'flex'
+    }
+}
